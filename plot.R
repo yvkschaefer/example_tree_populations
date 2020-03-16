36344ffbf1d26f7c529f53a04d15a557374e20d5
Kara Schaefer <yvkschaefer@gmail.com>
2020-03-16 11:00:47 -0700

    attempt to rm unnecessary .Rproj.user folder

--- plot.R
+++ plot.R
@@ -14,6 +14,7 @@ overall_mean <- mean(population_a_mean,
                      population_b_mean,
                      population_c_mean,
                      population_d_mean)
+# pull this from real (fake for now) data
 overall_sd <- mean(population_a_sd,
                    population_b_sd,
                    population_c_sd,
@@ -28,3 +29,15 @@ p9_3 <- ggplot(data.frame(x = c(0, 25)), aes(x = x)) +
   xlab("Temperature") +
   ylab("Growth")
 p9_3
+
+# print ggplot2 data visualization cheat sheet
+# stat_function is useful for overlaying functions
+set.seed(1492)
+ggplot(data.frame(x = rnorm(100)), aes(x)) +
+  geom_density() +
+  stat_function(fun = dnorm, colour = "red")
+
+
+ggplot(diamonds, aes(carat)) +
+  geom_density()
+
