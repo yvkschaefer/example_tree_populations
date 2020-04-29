d2d3e9befe032f316fa270731a74ea25fa033c93
Kara Schaefer <yvkschaefer@gmail.com>
2020-04-28 19:01:21 -0600

    update graphs

--- plot.R
+++ plot.R
@@ -1,31 +1,20 @@
 library(ggplot2)
 
+a <- seq(0, 20, by = .1)
+b <- seq(20, 40, by = .1)
+c <- seq(40, 60, by = .1)
+d <- seq(60, 80, by = .1)
 
-population_a_mean <- 10
-population_a_sd <- 3.3
-population_b_mean <- 12
-population_b_sd <- 3
-population_c_mean <- 15
-population_c_sd <- 2.5
-population_d_mean <- 20
-population_d_sd <- 3.3
-
-overall_mean <- mean(population_a_mean,
-                     population_b_mean,
-                     population_c_mean,
-                     population_d_mean)
+overall_mean <- mean(c(a, b, c, d))
 # pull this from real (fake for now) data
-overall_sd <- mean(population_a_sd,
-                   population_b_sd,
-                   population_c_sd,
-                   population_d_sd)
+overall_sd <- sd(c(a, b, c, d))
 
-p9_3 <- ggplot(data.frame(x = c(0, 25)), aes(x = x)) +
-  stat_function(fun = dnorm, args = list(population_a_mean, population_a_sd)) +
-  stat_function(fun = dnorm, args = list(population_b_mean, population_b_sd)) +
-  stat_function(fun = dnorm, args = list(population_c_mean, population_c_sd)) +
-  stat_function(fun = dnorm, args = list(population_d_mean, population_d_sd)) + 
-  stat_function(fun = dnorm, args = list(overall_mean, overall_sd), colour = "red", linetype="dashed") + 
+p9_3 <- ggplot(data.frame(x = c(-10, 90)), aes(x = x)) +
+  stat_function(fun = dnorm, args = list(mean(a), sd(a))) +
+  stat_function(fun = dnorm, args = list(mean(b), sd(b))) +
+  stat_function(fun = dnorm, args = list(mean(c), sd(c))) +
+  stat_function(fun = dnorm, args = list(mean(d), sd(d))) +
+  stat_function(fun = dnorm, args = list(overall_mean, overall_sd), colour = "red", linetype="dashed") +
   xlab("Temperature") +
   ylab("Growth")
 p9_3
@@ -41,3 +30,43 @@ ggplot(data.frame(x = rnorm(100)), aes(x)) +
 ggplot(diamonds, aes(carat)) +
   geom_density()
 
+
+# calculate the means
+iris.mean <- aggregate(x = iris, by = list(iris$Species), FUN = mean)
+# create a simple barplot
+barplot(iris.mean$Sepal.Length)
+
+
+beaver.mean <- aggregate(x = beaver1, by = list(beaver1$day), FUN = mean)
+beaver_2.mean <- aggregate(x = beaver2, by = list(beaver2$day), FUN = mean)
+
+tree <- rbind(beaver.mean, beaver_2.mean)
+names(tree) <- c("Population", "del", "Growth", "Temperature", "Activity")
+tree$del <- NULL
+barplot(tree$Population)
+
+
+
+# dnorm - this function gives height of the probability distribution at each point for a given mean and standard deviation
+x <- seq(-10, 10, by = .1)
+y <- dnorm(x, mean = 2.5, sd = 0.5)
+plot(x, y)
+
+# pnorm - this function gives the probability of a normally distributed random number to be less than the value of a given number. It is also called "Cumulative Distribution Function"
+x <- seq(-10, 10, by = 0.2)
+y <- pnorm(x, mean = 2.5, sd = 2)
+plot(x, y)
+
+
+# qnorm - this function takes the probability value and gives a number whose cumulative value matches the probability value
+x <- seq(0, 1, by = 0.02)
+y <- qnorm(x, mean = 2, sd = 1)
+plot(x, y)
+
+
+# rnorm - this function is used to generate random numbers whose distribution is normal. It takes the sample size as input and generates that many random numbers. We draw a histogram to show the distribution of the generated numbers
+
+# create a sample of 50 numbers which are normally distributed
+y <- rnorm(50)
+
+hist(y, main = "Normal Distribution")
