644aaa54ddb2af63eb884f35e8dbe66455464142
Kara Schaefer <yvkschaefer@gmail.com>
2020-05-06 17:00:54 -0600

    merge all histograms into a single graphic each line is identified by population y axis, x axis, and title updated formatted with theme_few

--- plot.R
+++ plot.R
@@ -1,3 +1,5 @@
+library(ggthemes)
+library(data.table)
 library(ggplot2)
 library(tidyverse)
 
@@ -61,7 +63,7 @@ hist(x1)
 
 set.seed(1) # sets a seed of reproducible results. used to produce the same sample again and again
 my_hist <- data.frame( # my_hist is a data frame with two columns, group as well as value. It has 700 rows
-  group = c(rep("A", 300), rep("B",150), rep("C",150), rep("D",100),rep("E",100)),
+  group = c(rep("A", 300), rep("B",150), rep("C",150), rep("D",100), rep("E",100)),
   value = c(rnorm(300, 30, 10), # rnorm has n, number of observations, mean, and standard deviation. 200, 20, 5
             # standard deviation is a measure of how spread out numbers are
             # is the square root of the variance
@@ -76,20 +78,38 @@ my_hist <- data.frame( # my_hist is a data frame with two columns, group as well
 bw = 2
 n_obs = sum(!is.na(my_hist$value))
 
+x <- seq(-25,100,0.1)
+mu <- 30
+sdev <- 15
+y <- dnorm(x, mu, sdev)
 
-my_hist %>% 
+# overall is the name of the group
+# xx is how many times that should be repeated
+# xx is length(x)
+
+
+data.frame(group = rep(overall,xx), value = x, y = y)
+# make a data frame
+# rbind that to the my_hist data frame
+
+my_hist %>%
   group_by(group) %>%
   nest(value) %>% # nesting creates a list-column of data frames, unnesting flattens it back out into regular columns
-# map() apply a function to each element of a vector. map() always returns a list
-# tilde is used to separate the left- and right-hand sides in a model formula. Our formula here describes the dnorm being multiplied by the bin_width and the sum of all the non_missing observations
-# dnorm takes a vector (.$value in this case), a mean, and standard deviation
+  # map() apply a function to each element of a vector. map() always returns a list
+  # tilde is used to separate the left- and right-hand sides in a model formula. Our formula here describes the dnorm being multiplied by the bin_width and the sum of all the non_missing observations
+  # dnorm takes a vector (.$value in this case), a mean, and standard deviation
   mutate(y = map(data, ~ dnorm(
     .$value, mean = mean(.$value), sd = sd(.$value)
-  ) * bw * sum(!is.na(.$value)))) %>% 
-  unnest(data,y) %>% 
-  
-  ggplot(aes(x = value)) +
-  geom_histogram(data = my_hist, binwidth = bw, alpha = 0) +
-  geom_line(aes(y = y), colour = "black") + 
-  facet_wrap(~ group)
+  ) * bw * sum(!is.na(.$value)))) %>%
+  unnest(data,y) %>%
+  ungroup() %>%
+  as.data.table() %>%
+  mutate(group = as.factor(group)) %>%
+  ggplot(aes(x = value, y = y, group = group, colour = group)) +
+  geom_line() +
+  xlab("Growing Degree Days") +
+  ylab("Growth Rate (mm)") +
+  ggtitle("Growth Rate of Species and Select Populations by Growing Degree Days") +
+  theme_few()
 
+ggsave("growth_rate.png")
